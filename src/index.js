f42ac2bab52bad69f70a2cb0a85501e743c8c7dc
ToryaToria <toria.torya@yandex.ru>
2025-08-12 07:36:44 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversedNum = Number([...Math.abs(n).toString()].reverse().join(''));
+  const isMinus = Math.sign(n);
+  return isMinus * reversedNum; // Сохраняем знак
 };
